cde12057cb983b8482fcdc718f6856609fde6d3d
Robbert Verbruggen <robbert.verbruggen@traxion.com>
2022-04-28 13:55:40 +0200

    ci: sonarcloud coverage

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -1,9 +1,33 @@
 import * as path from 'path';
 import * as Mocha from 'mocha';
 import * as glob from 'glob';
+import { join } from "path";
 
-export function run(): Promise<void>
+
+function setupCoverage()
 {
+	const NYC = require('nyc');
+	const nyc = new NYC({
+		cwd: join(__dirname, '..', '..', '..'),
+		exclude: ['**/test/**', '.vscode-test/**'],
+		reporter: ['text', 'lcov'],
+		all: true,
+		instrument: true,
+		hookRequire: true,
+		hookRunInContext: true,
+		hookRunInThisContext: true,
+	});
+
+	nyc.reset();
+	nyc.wrap();
+
+	return nyc;
+}
+
+export async function run(): Promise<void>
+{
+	const nyc = setupCoverage();
+
 	// Create the mocha test
 	const mocha = new Mocha({
 		ui: 'tdd',
@@ -12,35 +36,26 @@ export function run(): Promise<void>
 
 	const testsRoot = path.resolve(__dirname);
 
-	return new Promise((c, e) =>
+	const options = { cwd: __dirname };
+	const files: string[] = glob.sync('**/*.test.js', options);
+
+	// Add files to the test suite
+	files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+
+
+	try
 	{
-		glob('**/**.test.js', { cwd: testsRoot }, (err, files) =>
+		await new Promise((resolve, reject) =>
 		{
-			if (err)
+			// Run the mocha test
+			mocha.run(failures =>
 			{
-				return e(err);
-			}
-
-			// Add files to the test suite
-			files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
-
-			try
-			{
-				// Run the mocha test
-				mocha.run(failures =>
-				{
-					if (failures > 0)
-					{
-						e(new Error(`${failures} tests failed.`));
-					} else
-					{
-						c();
-					}
-				});
-			} catch (error)
-			{
-				e(error);
-			}
+				failures ? reject(new Error(`${failures} tests failed.`)) : resolve(undefined);
+			});
 		});
-	});
+	} finally
+	{
+		nyc.writeCoverageFile();
+		await nyc.report();
+	}
 }
