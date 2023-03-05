e104fb7584547a02367bd580fdf48853091fe8f0
Robbert Verbruggen <verbruggenrf@outlook.com>
2023-03-05 08:03:54 +0100

    fix: update imports

--- src/providers/GuidCodeLensProvider.ts
+++ src/providers/GuidCodeLensProvider.ts
@@ -1,39 +1,33 @@
 import * as vscode from 'vscode';
-import { TeamMember } from "./models/TeamMember";
+import { TeamMember } from "../models/TeamMember";
 
-export class GuidCodeLensProvider implements vscode.CodeLensProvider
-{
+export class GuidCodeLensProvider implements vscode.CodeLensProvider {
 	private codeLenses: vscode.CodeLens[] = [];
 	private regex: RegExp;
 	private _onDidChangeCodeLenses: vscode.EventEmitter<void> = new vscode.EventEmitter<void>();
 	public readonly onDidChangeCodeLenses: vscode.Event<void> = this._onDidChangeCodeLenses.event;
 
-	constructor()
-	{
+	constructor() {
 		this.regex = /@<([a-zA-Z0-9\-]+)>/g;
 
-		vscode.workspace.onDidChangeConfiguration((_) =>
-		{
+		vscode.workspace.onDidChangeConfiguration((_) => {
 			this._onDidChangeCodeLenses.fire();
 		});
 	}
 
-	public provideCodeLenses(document: vscode.TextDocument, _token: vscode.CancellationToken): vscode.CodeLens[] | Thenable<vscode.CodeLens[]>
-	{
+	public provideCodeLenses(document: vscode.TextDocument, _token: vscode.CancellationToken): vscode.CodeLens[] | Thenable<vscode.CodeLens[]> {
 		this.codeLenses = [];
 		const regex = new RegExp(this.regex);
 		const text = document.getText();
 		let matches;
 
-		while ((matches = regex.exec(text)) !== null)
-		{
+		while ((matches = regex.exec(text)) !== null) {
 			const line = document.lineAt(document.positionAt(matches.index).line);
 			const indexOf = line.text.indexOf(matches[0]);
 			const position = new vscode.Position(line.lineNumber, indexOf);
 			const range = document.getWordRangeAtPosition(position, new RegExp(this.regex));
 
-			if (range)
-			{
+			if (range) {
 				let teamMembers: TeamMember[] = vscode.workspace.getConfiguration("azdo-teammembers").get("teammembers", []);
 
 				let guid = matches[1];
@@ -42,8 +36,7 @@ export class GuidCodeLensProvider implements vscode.CodeLensProvider
 				let name = teamMembers.find(member => member.guid.toUpperCase() === guid.toUpperCase())?.name;
 
 				// Only insert a code lense if a name is found.
-				if (name !== undefined)
-				{
+				if (name !== undefined) {
 					let command: vscode.Command = {
 						title: name,
 						tooltip: name,
--- src/providers/GuidCompletionItemProvider.ts
+++ src/providers/GuidCompletionItemProvider.ts
@@ -1,16 +1,13 @@
 import * as vscode from 'vscode';
-import { TeamMember } from "./models/TeamMember";
+import { TeamMember } from "../models/TeamMember";
 
-export class GuidCompletionItemProvider implements vscode.CompletionItemProvider
-{
-	public provideCompletionItems(_document: vscode.TextDocument, _position: vscode.Position, _token: vscode.CancellationToken, _context: vscode.CompletionContext): vscode.ProviderResult<vscode.CompletionItem[] | vscode.CompletionList<vscode.CompletionItem>>
-	{
+export class GuidCompletionItemProvider implements vscode.CompletionItemProvider {
+	public provideCompletionItems(_document: vscode.TextDocument, _position: vscode.Position, _token: vscode.CancellationToken, _context: vscode.CompletionContext): vscode.ProviderResult<vscode.CompletionItem[] | vscode.CompletionList<vscode.CompletionItem>> {
 		let items: vscode.CompletionItem[] = [];
 
 		let teamMembers: TeamMember[] = vscode.workspace.getConfiguration("azdo-teammembers").get("teammembers", []);
 
-		teamMembers.forEach(member =>
-		{
+		teamMembers.forEach(member => {
 			const item = new vscode.CompletionItem(
 				member.name,
 				vscode.CompletionItemKind.Text
